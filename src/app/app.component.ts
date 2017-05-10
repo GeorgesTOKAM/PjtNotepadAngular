4358625a39dda451fcd03e4f8522df00c94e85c4
GeorgesTokam <Georgestokam@live.fr>
2017-05-10 15:28:12 +0200

    Version finale de Angular2Notepad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,38 @@
 import { Component } from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
+
+
+import {FormBuilder, FormGroup, Validators, FormControl, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<div class="container-fluid">
+    <div class="col-md-10">
+    <div class="row">
+      <div class="col-md-6"><br><img src="assets/Angular.png" alt="Photo de montagne" class="img-rounded"><br><br></div>
+    </div>
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded-info" style="background-color: #f6f7ed;">
+            
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item"><a class="nav-link" routerLink="/Notes">Notes</a></li>
+        <li class="nav-item"><a class="nav-link" routerLink="/Categories" (click) = "gotocategories">Catégories</a></li>
+        <li class="nav-item"><a class="nav-link" routerLink="/AddNotes">Ajouter une Note</a></li>
+        <li class="nav-item"><a class="nav-link" routerLink="/Addcategorie">Ajouter une catégorie</a></li>
+        <li class="nav-item"><a class="nav-link" routerLink="/about">About</a></li>
+      </ul>  
+      
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-1" type="text" placeholder="Search">
+        <button class="btn btn-outline-success my-0 my-sm-0" type="submit">Search</button>
+      </form>
+    </nav>
+    <router-outlet></router-outlet>
+    </div>
+  </div>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'Angular NotePad';
+}
